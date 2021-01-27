6aca86762210f27c27805fb5030c13deff8b97a3
DESKTOP-V8UD4Q9\Onini <butsakorn.buts@bumail.net>
2021-01-28 01:13:02 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Week1/Week1_1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Week1_1 : MonoBehaviour
+{
+    public Text Shing;
+    string animal ;
+    public Text Showtext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void inventory()
+    {
+        animal = Shing.text;
+
+        switch (animal)
+        {
+            case "Cat": 
+               Showtext.text = animal + " is found";
+                break;
+            case "Dog":
+                Showtext.text = animal + " is found";
+                break;
+            case "Fish":
+                Showtext.text = animal + " is found";
+                break;
+            case "Lion":
+                Showtext.text = animal + " is found";
+                break;
+            case "Bee":
+                Showtext.text = animal + " is found";
+                break;
+            case "Wolf":
+                Showtext.text = animal + " is found";
+                break;
+            default:
+                Showtext.text = animal + " is not found";
+                break;
+        }
+
+
+
+    }
+
+}
